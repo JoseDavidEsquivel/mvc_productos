9d746a005466fbef9c03ef7b2ebee591a61175dd
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2024-03-08 11:40:05 -0600

    1.0.a1 paginacion implementada en el listado de index

--- aplicacion/mvc/controllers/listar_productos.py
+++ aplicacion/mvc/controllers/listar_productos.py
@@ -1,38 +1,13 @@
 # import web
-# import sqlite3
-# # from mvc.models.m_listar import ModeloProductos
-# from pydantic import BaseModel
-
-# conn = sqlite3.connect("sql/productos.db")
-
-# class Producto(BaseModel):
-#     id: int
-#     nombre: str
-#     descripcion: str
-#     precio: int
-#     existencias: int
+# from mvc.models.m_listar import ModeloProductos
 
 # render = web.template.render('mvc/views/', base='layout')
 
 # class Listar:
 #     def GET(self):
-#         """Lista los productos existentes, en un rango de 50 registros"""
-#         c = conn.cursor()
-#         c.execute('SELECT * FROM productos;')
-#         response = []
-#         for row in c.fetchall():
-#             producto = Producto(id=row[0], nombre=row[1], descripcion=row[2], precio=row[3], existencias=row[4])
-#             lista = {
-#                 'id': row[0],
-#                 'nombre': str(row[1]), 
-#                 'descripcion': str(row[2]), 
-#                 'precio': row[3], 
-#                 'existencias': row[4]
-#                 }
-            
-#             response.append(lista)
-#             print(response)
-#         return render.listar(response)
+#         producto_controller = ModeloProductos()
+#         productos = producto_controller.listar_productos()
+#         return render.listar(productos)
 
 import web
 from mvc.models.m_listar import ModeloProductos
@@ -41,6 +16,8 @@ render = web.template.render('mvc/views/', base='layout')
 
 class Listar:
     def GET(self):
+        i = web.input(page=1)
+        page = int(i.page)
         producto_controller = ModeloProductos()
-        productos = producto_controller.listar_productos()
-        return render.listar(productos)
+        productos = producto_controller.listar_productos(page)
+        return render.listar(productos, page=page)
--- aplicacion/mvc/models/m_listar.py
+++ aplicacion/mvc/models/m_listar.py
@@ -1,61 +1,3 @@
-import web
-# import sqlite3
-# from pydantic import BaseModel
-
-# conn = sqlite3.connect("sql/productos.db")
-
-# class Producto(BaseModel):
-#     id: int
-#     nombre: str
-#     descripcion: str
-#     precio: int
-#     existencias: int
-
-# class ModeloProductos:
-#     async def __init__(self):
-#         """Lista los productos existentes, en un rango de 50 registros"""
-#         c = conn.cursor()
-#         c.execute('SELECT * FROM productos;')
-#         response = []
-#         for row in c.fetchall():
-#             producto = Producto(id=row[0], nombre=row[1], descripcion=row[2], precio=row[3], existencias=row[4])
-#             response.append(producto)
-#         self.lista = response
-#         await self.lista
-
-# class ModeloProductos:
-#     def __init__(self):
-#         self.lista = [
-#             {
-#                 'id': 1,
-#                 'nombre': 'Laptop Intel', 
-#                 'descripcion': 'Laptop potente', 
-#                 'precio': 1500, 
-#                 'existencias': 30
-#             },
-#             {
-#                 'id': 1,
-#                 'nombre': 'Laptop Intel', 
-#                 'descripcion': 'Laptop potente', 
-#                 'precio': 1500, 
-#                 'existencias': 30
-#             },
-#             {
-#                 'id': 1,
-#                 'nombre': 'Laptop Intel', 
-#                 'descripcion': 'Laptop potente', 
-#                 'precio': 1500, 
-#                 'existencias': 30
-#             },
-#             {
-#                 'id': 1,
-#                 'nombre': 'Laptop Intel', 
-#                 'descripcion': 'Laptop potente', 
-#                 'precio': 1500, 
-#                 'existencias': 30
-#             },
-#         ]
-
 
 import mysql.connector
 
@@ -69,8 +11,9 @@ class ModeloProductos:
         )
         self.cursor = self.conn.cursor()
 
-    def listar_productos(self):
-        self.cursor.execute("SELECT * FROM productos LIMIT 25")
+    def listar_productos(self, page):
+        offset = (page - 1) * 25
+        self.cursor.execute("SELECT * FROM productos LIMIT 25 OFFSET %s", (offset,))
         productos = self.cursor.fetchall()
 
         response = []
